184369bfec96d74e859ffb9a0b8d0f9550e5ef9a
matheus soares <matheusoares2604@gmail.com>
2022-03-24 13:52:47 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,113 @@
+/* let nome = prompt("Digite seu nome");
+
+alert("seu nome é " + nome)
+console.log(nome)
+
+
+let number1 = parseInt (prompt("Digite um numero"));
+let number2 = parseInt (prompt("Digite outro numero"))
+
+console.log( number1 + number2)
+
+
+let nota1 = parseFloat(prompt("Digite sua primeira nota"));
+let nota2 = parseFloat(prompt("Digite sua segunda nota"))
+console.log((nota1 + nota2)/2); */
+
+/*let num = parseInt(prompt("Digite um numero"))
+
+if ( num % 2==0 ){
+    console.log("Este numero é par")
+} else{
+    console.log("Este numero é impar")
+}*/
+
+/*let num = parseInt(prompt("Digite um numero"))
+
+if ( >= <=){
+    console.log("Este numero é par")
+} else{
+    console.log("Este numero é impar")
+}*/
+
+/*let genero = prompt("Digite seu genero");
+
+if(genero === "F" || genero === "f") {
+    console.log(" feminino")
+} else if (genero === "M" || genero ==="m"){
+    console.log("Masculino")
+} else {
+    console.log("Genero Invalido")
+}
+
+let numero = prompt("digite um numero");
+
+let F= numero;
+let C = (5*(F-32)) /9;
+
+console.log("Farenheint", numero);
+console.log("Graus", C)
+
+let nota1 = parseInt(prompt("Digita a sua primeira nota"))
+let nota2 = parseInt(prompt("Digita a sua segunda nota"))
+
+let media = ((nota1 + nota2)/2)
+console.log(media)     
+
+if(media === 10){
+         console.log("Aprovado com distinção")
+} else if (media < 7){
+         console.log("Reprovado")
+
+} else if(media >= 7){
+         console.log("Aprovado")
+     }
+
+let number1 = parseInt(prompt("Digite o primeiro numero"))
+let number2 = parseInt(prompt("Digite o segundo numero"))
+let number3 = parseInt(prompt("Digite o terceiro numero"))
+
+let numMaior = Math.max(number1,number2,number3)
+let numMenor = Math.min(number1,number2,number3)
+
+alert("O maior número é: "+numMaior+" e o menor número é: "+numMenor)
+
+/*let numero = (prompt("Digite um numero"))
+
+if (numero >= 0){
+    console.log("Este numero é positivo")
+} else{
+    console.log("Este numero é negativo")
+}*/
+
+
+/*let numero = (prompt("Digite o primeiro numero"))
+let numero2 = (prompt("Digite o segundo numero"))
+
+if (numero >= numero2){
+    console.log("O primeiro numero é maior que o segundo numero")
+} else{
+    console.log("O segundo numero é maior que o primeiro numero")
+}/*
+
+/*let hora = parseInt(prompt("Digite quanto voce recebe por hora")) 
+let numberHoras = parseInt(prompt("Digite quantas horas voce trabalha")) 
+console.log(hora * numberHoras)
+
+
+let ano = parseInt(prompt("Digite o ano em que voce nasceu"))
+console.log(2022 - ano);
+
+let nota1 = parseFloat(prompt("Digite sua primeira nota"));
+let nota2 = parseFloat(prompt("Digite sua segunda nota"))
+console.log((nota1 + nota2)/2);
+
+let number1 = parseInt (prompt("Digite um numero"));
+let number2 = parseInt (prompt("Digite outro numero"))
+
+console.log( number1 + number2)*/
+
+/*let nome = prompt("Digite seu nome"); /*mostra uma caixa para o usuario digitar*/
+
+/*alert("seu nome é " + nome) /*exibe uma caixa de mensagem com a mensagem que esta dentro do ()*/
+/*console.log(nome) /*exibe no devtools*/
